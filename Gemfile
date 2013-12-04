063e9fc837fed7c7d51c92e0aab9d2f920d8fcac
Rob Nichols <git@nicholshayes.co.uk>
2013-12-04 11:34:43 +0000

    Initial rails app

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+
+gem 'mysql2'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Midashboard::Application.config.secret_token = 'aa4f5d068e91bc13aece4c1ec9b95039cb6ef2dd14683f47aebca66c4bfe828a46f1e246c106111e57ba8c2d9fea9ed6bbb02bea1b67e07ee3c2d4b9b3192177'
