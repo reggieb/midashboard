684945b2c11df75eb92b0b5b5322e997ee6bef9b
Rob Nichols <git@nicholshayes.co.uk>
2013-12-18 10:37:28 +0000

    Adds user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+email = 'robnichols@warwickshire.gov.uk'
+unless User.exists?(:email => email)
+  password = 'change_me'
+  User.create!(
+    :email => email,
+    :password => password,
+    :password_confirmation => password
+  )
+  puts "User added"
+end
